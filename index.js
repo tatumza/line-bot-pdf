1515571053ca29d779b0f07729d349d90bb18bdf
tatumza <44038520+tatumza@users.noreply.github.com>
2025-07-26 22:07:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/webhook', async (req, res) => {
     if (event.type === 'message' && event.message.type === 'text') {
       const text = event.message.text.trim();
 
-      if (text === '1') {
+      if (text === 'A') {
         // ตอบกลับเป็นไฟล์ PDF
         await axios.post(
           'https://api.line.me/v2/bot/message/reply',
